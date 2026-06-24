5849465830ca6244200073edfb99d0ccd8d3b9b3
miaoxu <miaoxu1com@users.noreply.github.com>
2026-06-25 00:56:36 +0800

    Update z.fish

--- z.fish
+++ z.fish
@@ -2,24 +2,25 @@ zoxide init fish | source
 # source (lua /home/mx/z.lua/z.lua --init fish once enhanced fzf | psub)
 fish_config theme choose catppuccin-frappe
 
-function cd --description "Change directory with fzf + fd"
-    if test (count $argv) -eq 0
-        set -l target_dir (
-            fd --type d --hidden --exclude .git . 2>/dev/null | \
-            fzf --height 40% --reverse --border \
-                --prompt="Select directory: " \
-                --preview="ls -la {} | head -20" \
-                --preview-window=right:50%
-        )
-
-        if test -n "$target_dir"
-            builtin cd "$target_dir"
-            commandline -f repaint
-        end
-    else
-        builtin cd $argv
-    end
-end
+# 与默认cd tab功能冲突
+# function cd --description "Change directory with fzf + fd"
+#     if test (count $argv) -eq 0
+#         set -l target_dir (
+#             fd --type d --hidden --exclude .git . 2>/dev/null | \
+#             fzf --height 40% --reverse --border \
+#                 --prompt="Select directory: " \
+#                 --preview="ls -la {} | head -20" \
+#                 --preview-window=right:50%
+#         )
+#
+#         if test -n "$target_dir"
+#             builtin cd "$target_dir"
+#             commandline -f repaint
+#         end
+#     else
+#         builtin cd $argv
+#     end
+# end
 
 function y
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
